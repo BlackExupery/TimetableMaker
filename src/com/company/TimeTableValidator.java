b9a136a8d623c1452d9c0edb034799b245ecf859
BlackExupery <xtuleanh@yahoo.de>
2018-05-09 12:42:39 +0200

    TimeTableValidator implementiert.

---
+++ src/com/company/TimeTableValidator.java
@@ -0,0 +1,93 @@
+package com.company;
+
+import java.util.List;
+import java.util.Map;
+
+public class TimeTableValidator {
+
+    private InputReader inputdata;
+    private OutputReader outputdata;
+
+    public TimeTableValidator(InputReader ir, OutputReader or){
+        this.inputdata = ir;
+        this.outputdata = or;
+    }
+
+    public boolean check_s_has_f_condition(){
+
+        Map<Long,List<String>> s_has_f = inputdata.get_map_s_has_f();
+        Map<Long,List<Long>> s_in_g = outputdata.get_s_in_g();
+        Map<Long,String> g_of_sbj= outputdata.get_g_of_sbj();
+
+        //gehe jeden studenten s durch
+        for(Long s : s_has_f.keySet()){
+
+            //gehe jedes fach f durch, den student s besucht durch
+            for(String sbj : s_has_f.get(s)){
+                int gs_of_f =0;
+                //gehe jede gruppe g durch
+                for(Long g : g_of_sbj.keySet()){
+
+                    //überprüfe ob g von fach f ist.
+                    if(g_of_sbj.get(g).equals(sbj)){
+                        //zähle wie viele gruppen g von fach f, student s besucht (Es muss genau 1 sein!)
+                        if(s_in_g.get(s).contains(g)){
+                            gs_of_f++;
+                        }
+                    }
+                }
+                // wenn student s in mehr oder weniger als einer Gruppe von fach f ist, in dem student s
+                // angemeldet ist, ist es ein fehler!
+                if(gs_of_f != 1){
+                    return false;
+                }
+
+            }
+
+        }
+        //wenn alle studenten fehlerfrei untersucht wurden, ist die s_in_g-Voraussetzung erfüllt.
+        return true;
+    }
+
+
+    public boolean check_s_rej_t_condition(){
+
+        Map<Long,List<String>> s_rej_t = inputdata.get_map_s_rejects_t();
+        Map<Long,List<Long>> s_in_g = outputdata.get_s_in_g();
+        Map<Long,String> g_in_t= outputdata.get_g_in_t();
+
+        //gehe jeden studenten s durch
+        for(Long s : s_rej_t.keySet()){
+
+            //gehe jeden timeslot t durch, den student s abgesagt hat durch
+            for(String t : s_rej_t.get(s)){
+                int gs_in_t =0;
+                //gehe jede gruppe g durch
+                for(Long g : g_in_t.keySet()){
+                    //überprüfe ob g in timeslot t ist.
+                    if(g_in_t.get(g).equals(t)){
+                        //zähle wie viele gruppen g in t, student s besucht (Es muss genau 0 sein!)
+                        if(s_in_g.get(s).contains(g)){
+                            gs_in_t++;
+                        }
+                    }
+                }
+                // wenn student s in mehr oder weniger als einer Gruppe von fach f ist, in dem student s
+                // angemeldet ist, ist es ein fehler!
+                if(gs_in_t != 0){
+                    return false;
+                }
+
+            }
+
+        }
+        //wenn alle studenten fehlerfrei untersucht wurden, ist die s_in_g-Voraussetzung erfüllt.
+        return true;
+    }
+
+   /* public boolean check_groupCapacity_condition(){
+
+    }*/
+
+
+}
